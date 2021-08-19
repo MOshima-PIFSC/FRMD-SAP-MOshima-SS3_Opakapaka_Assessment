d09a82aabdd27f430f4e126cdf72d26311ced54a
MOshima-PIFSC <megumi.oshima@noaa.gov>
2021-08-19 12:45:08 -0400

    updated data figs

--- R_scripts/Data_sources_N.R
+++ R_scripts/Data_sources_N.R
@@ -1,8 +1,13 @@
 library(tidyverse)
 library(nmfspalette)
 
+#BFISH datasets
 camera_lengths <- read.csv("./Data/BFISH 2016-2019 Camera Lengths.csv")
 fishing_lengths <- read.csv("./Data/BFISH 2016-2019 Research Fishing Lengths.csv")
+
+#Life-history data
+lh <- read.csv("./FRMD-SAP-MOshima-SS3_Opakapaka_Assessment/Data/Life-history database.csv")
+
 #FRS data broken up into 3 datasets
 early <- read.csv("./FRMD-SAP-MOshima-SS3_Opakapaka_Assessment/Data/Data for Meg/picdr_112849_fy48_15.csv")
 late <- read.csv("./FRMD-SAP-MOshima-SS3_Opakapaka_Assessment/Data/Data for Meg/picdr_112849_fy16_18.csv")
@@ -11,15 +16,15 @@ areas_frs <- read.csv("./FRMD-SAP-MOshima-SS3_Opakapaka_Assessment/Data/Data for
 areas_frs <- areas_frs %>% 
   filter(Valid. == "") %>% 
   select(-Valid.)
-
-
 frs <- rbind(early, late, d2019)
 head(frs)
 
-### Double check with john that com_names is the same order as species id 
+#DRS data 2000-2021
+deep7_drs <- read.csv("./FRMD-SAP-MOshima-SS3_Opakapaka_Assessment/Data/Data for Meg/DRS_deep7.csv")
+
 com_names <- c("Hapuupuu","Kalekale","Opakapaka","Ehu","Onaga","Ehu","Lehi","Gindai")
 frs_id <- data.frame(COMMON_NAME = com_names,  SPECIES = c(15,17,19,21,22,36,58,97))
-
+deep7 <- unique(camera_lengths$COMMON_NAME)[-c(8:9)]
 
 deep7_frs <- frs %>% 
   filter(SPECIES %in% frs_id$SPECIES) %>% 
@@ -30,14 +35,11 @@ deep7_frs <- frs %>%
   select(SPECIES, HOURS, CAUGHT, LBS, NUM_SOLD, LBS_SOLD, F_SEASON, F_LBS, CYEAR, FYEAR) %>% 
   left_join(frs_id, by = "SPECIES")
 
-deep7 <- unique(camera_lengths$COMMON_NAME)[-c(8:9)]
-  
-
 cam_sum <- camera_lengths %>% 
   filter(COMMON_NAME %in% deep7) %>% 
   select(COMMON_NAME, OFFICIAL_DEPTH_M, LENGTH_CM, BFISH) %>% 
   mutate(Year = as.integer(str_extract_all(BFISH, "[0-9]+", simplify = TRUE))) %>% 
-  group_by(COMMON_NAME) %>% #for first figure remove Year from group_by
+  group_by(COMMON_NAME, Year) %>% #for first figure remove Year from group_by
   summarise(N = n()) %>% 
   mutate(Data_Source = "BFISH_camera_length")
 
@@ -45,19 +47,50 @@ rfish_sum <- fishing_lengths %>%
   filter(COMMON_NAME %in% deep7) %>% 
   select(COMMON_NAME, LENGTH_CM, BFISH) %>% 
   mutate(Year = as.integer(str_extract_all(BFISH, "[0-9]+", simplify = TRUE))) %>% 
-  group_by(COMMON_NAME) %>% 
+  group_by(COMMON_NAME, Year) %>% 
   summarise(N = n()) %>% 
   mutate(Data_Source = "BFISH_fishing_length")
 
 frs_sum <- deep7_frs %>% 
-  group_by(COMMON_NAME) %>% 
+  group_by(COMMON_NAME, FYEAR) %>% 
   rename(Year = FYEAR) %>% 
   summarise(N = n()) %>% 
   mutate(Data_Source = "FRS_lbs_caught",
          N = N/10) 
 
+drs_sum <- deep7_drs %>% 
+  rename("COMMON_NAME" = "common_name") %>% 
+  group_by(COMMON_NAME, Year) %>% 
+  summarise(N = n()) %>% 
+  mutate(Data_Source = "DRS_lbs_caught",
+         N = N/10)
 
-data.df <- bind_rows(cam_sum, rfish_sum, frs_sum) %>% 
+lh_sum <- lh %>% 
+  mutate(COMMON_NAME = str_replace_all(TAXONNAME, 
+                                       c("Pristopomoides filamentosus" = "Opakapaka",
+                                         "Pristipomoides filamentosus" = "Opakapaka",
+                                         "Hyporthodus quernus" = "Hapuupuu",
+                                         "Etelis coruscans" = "Onaga",
+                                         "Etelis carbunculus" = "Ehu",
+                                         "Pristipomoides sieboldii" = "Kalekale",
+                                         "Apareus rutilans" = "Lehi",
+                                         "Aphareus rutilans" = "Lehi",
+                                         "Pristopomoides zonatus" = "Gindai",
+                                         "Pristipomoides zonatus" = "Gindai"))) %>% 
+  filter(str_detect(LOCATION, "HI|Hawaii")) %>% 
+  select(Year, COMMON_NAME, N, N.1, N.2) %>% 
+  rename("Age-Length" = "N",
+         "Maturity" = "N.1",
+         "Length-Weight" = "N.2") %>% 
+  pivot_longer(cols = -c(COMMON_NAME, Year), names_to = "Data_Source", values_to = "N") %>% 
+  filter(!is.na(N)) %>% 
+  group_by(COMMON_NAME, Year, Data_Source) %>% 
+  summarise(N = n()) %>% 
+  mutate(N = N*5) %>% 
+  select(COMMON_NAME, Year, N, Data_Source)
+  
+
+data.df <- bind_rows(cam_sum, rfish_sum, frs_sum, drs_sum, lh_sum) %>% 
   mutate(COMMON_NAME = factor(COMMON_NAME, 
                               levels = c("Opakapaka", 
                                          "Onaga", 
@@ -66,7 +99,13 @@ data.df <- bind_rows(cam_sum, rfish_sum, frs_sum) %>%
                                          "Hapuupuu", 
                                          "Gindai", 
                                          "Lehi")),
-         Data_Source = factor(Data_Source)) %>% 
+         Data_Source = factor(Data_Source, levels = c("BFISH_camera_length", 
+                                                      "BFISH_fishing_length",
+                                                      "FRS_lbs_caught",
+                                                      "DRS_lbs_caught",
+                                                      "Age-Length",
+                                                      "Length-Weight",
+                                                      "Maturity"))) %>% 
   group_by(Data_Source) 
 
 #get x coordinates for text labels to the side
@@ -76,12 +115,13 @@ x.pos <- group_indices(data.df)
 ##
 ggplot(data = data.df, aes(x = Data_Source, y = COMMON_NAME, alpha = N)) + 
   geom_point(aes(size = N+5), color = "#0093D0", show.legend = FALSE) +
-  geom_text(aes(label = N, x = x.pos + .25), alpha = 1.0, size = 4, color = "#0093D0") +
+  geom_text(aes(label = N, x = x.pos + .23), alpha = 1.0, size = 4, color = "#0093D0") +
   scale_alpha_continuous(range = c(0.3, 0.7)) + 
   scale_size_area(max_size = 20) +
+  scale_x_discrete(guide = guide_axis(n.dodge = 2))+
   labs(x = "Data Source", 
        y = "Common Name",
-       caption = "Amount of data available per species (Nrows). Note FRS_lbs_caught is x10.") +
+       caption = "Amount of data available per species (Nrows). Note FRS_lbs_caught and DRS_lbs_caught are x10.") +
   theme_classic()  +
   ggsave(filename = "./FRMD-SAP-MOshima-SS3_Opakapaka_Assessment/Data/Data_sources_N.png")
 
